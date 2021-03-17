292d417434c8fe98254fdd1da73595a202868dbc
JonaRizvanolli <jonarizvanolli10@gmail.com>
2021-03-17 17:25:04 +0100

    landing page

---
+++ src/AppBundle/Controller/ButterflyController.php
@@ -0,0 +1,24 @@
+<?php
+
+
+namespace AppBundle\Controller;
+use AppBundle\Entity\Role;
+use AppBundle\Form\LoginAdminType;
+use AppBundle\Form\UserRegisterType;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use AppBundle\Entity\Biznes;
+
+class ButterflyController extends Controller
+{
+    /**
+     * @Route("/home", name="landingpage")
+     */
+
+    public function indexAction(Request $request)
+{
+    return $this->render('default/index0.html.twig');
+}
+}
+
